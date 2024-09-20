a5cc4f9df3a050928b9750a50a1fedcaf7a97ecb
erikmarquesfsa <erikmarquesfsa@gmail.com>
2024-09-20 20:40:08 -0300

    d

--- site/menu.php
+++ site/menu.php
@@ -2,8 +2,8 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
-        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold text-white">Material Dashboard 2</span>
+        <img src="" class="navbar-brand-img h-100" alt="">
+        <span class="ms-1 font-weight-bold text-white">Baratino</span>
       </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
@@ -86,11 +86,6 @@
         </li>
       </ul>
     </div>
-    <div class="sidenav-footer position-absolute w-100 bottom-0 ">
-      <div class="mx-3">
-        <a class="btn btn-outline-primary mt-4 w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard?ref=sidebarfree" type="button">Documentation</a>
-        <a class="btn bg-gradient-primary w-100" href="https://www.creative-tim.com/product/material-dashboard-pro?ref=sidebarfree" type="button">Upgrade to pro</a>
-      </div>
-    </div>
+   
   </aside>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
