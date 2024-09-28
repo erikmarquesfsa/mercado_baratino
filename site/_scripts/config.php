3cc2a9ca6b65c5dd7308cc6b41b46e0f9bd4992f
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-09-27 21:09:56 -0300

    .
    .

---
+++ site/_scripts/config.php
@@ -0,0 +1,10 @@
+<?php
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'mercado_b';
+$porta = '3306';
+
+$mysqli = new mysqli($servidor,$usuario,$senha,$banco,$porta);
+
+?>
--- site/_scripts/login.php
+++ site/_scripts/login.php
@@ -1,16 +1,28 @@
 <?php
+include "config.php";
+
 $login = $_POST['email'];
 $senha = $_POST['senha'];
 
-if($login=="teste"){
+$sql = "SELECT id FROM login WHERE login = '$login'";
+$query = $mysqli->query($sql);
+$total = $query->num_rows;
 
-    if($senha=="1234"){
-        echo "Usuário ok!";
+if($total==1){
+
+    $sql2 = "SELECT id FROM login WHERE login = '$login' and senha = '$senha'";
+    $query2 = $mysqli->query($sql2);
+    $total2 = $query2->num_rows;
+
+    if($total2==1){
+        echo "<script>alert('Usuário ok');</script>";
     }else{
-        echo "Verique sua senha";
+        echo "<script>alert('Verifique sua senha');
+        window.location.href=('../index.php');</script>";
     }
 }else{
-    echo "Verifique seu usuário";
+    echo "<script>alert('Verifique seu login');
+    window.location.href=('../index.php');</script>";
 }
 
 ?>
