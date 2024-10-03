3bcd40bae485a6df063c32f9da51294fb7e94133
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2024-10-03 19:36:12 -0300

    .
    .

--- site/_scripts/login.php
+++ site/_scripts/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <body>
 <?php
